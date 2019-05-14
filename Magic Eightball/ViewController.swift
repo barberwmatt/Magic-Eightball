6522760703d54862f11d16c39b81c1be098f88e0
Matthew W P Barber <barberwmatt@Edwards-Mac-mini.local>
2019-05-14 15:02:28 -0400

    Finish code

--- Magic Eightball/ViewController.swift
+++ Magic Eightball/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let ballArray: Array = [ "ball1", "ball2", "ball3", "ball4", "ball5" ]
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let random1 = Int.random( in: 0 ... 4)
+        imageView.image = UIImage(named: ballArray[random1])
     }
 
-
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        let random1 = Int.random( in: 0 ... 4)
+        imageView.image = UIImage(named: ballArray[random1])
+    }
+    
+    
+    
 }
 
