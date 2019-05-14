7ff5766e620baace91c04f25e888b9977fd5badf
Matthew W P Barber <barberwmatt@Edwards-Mac-mini.local>
2019-05-14 14:05:44 -0400

    Initial Commit

---
+++ Magic Eightball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Eightball
+//
+//  Created by Matthew W P Barber on 5/14/19.
+//  Copyright © 2019 Matthew W P Barber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
